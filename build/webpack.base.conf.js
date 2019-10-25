20f33eaea51be4b4109c4bb0ede37036a9ac51a2
Алексанов Анастас Александрович <aaale123@mts.ru>
2019-10-25 16:33:48 +0300

    добавление vue

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,11 +2,12 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require('vue-loader')
 
 const PATHS = {
-  src: path.join(__dirname, './src'),
-  dist: path.join(__dirname, './dist'),
-  assets: 'assets/ '
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../public'),
+  assets: 'static/ '
 }
 
 module.exports = {
@@ -23,6 +24,15 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loader: {
+            scss: 'vue-style-loader!css-loader!sass-loader'
+          }
+        }
+      },
       {
         test: /\.js$/,
         loader: "babel-loader",
@@ -42,9 +52,7 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
-            options: {
-              sourceMap: true
-            }
+            options: {sourceMap: true}
           },
           {
             loader: "postcss-loader",
@@ -81,12 +89,17 @@ module.exports = {
       }
     ] 
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.js'
+    }
+  },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: `${PATHS.assets}css/[name].css`,
-      chunkFilename: '[id].css',
     }),
     new HtmlWebpackPlugin({
       hash: false,
--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ import './scss/main.scss';
 // import Vue from 'vue';
 
 window.Vue = require('vue')
+
+Vue.component('example-component', require('./components/Example.vue').default)
+
+const app = new Vue({
+  el: '#app'
+})
